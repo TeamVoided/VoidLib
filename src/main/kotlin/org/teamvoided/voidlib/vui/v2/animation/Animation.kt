734542379a53cea49735d46d1ae45f707e3336b7
BrokenFuse <brokenfuse.self@gmail.com>
2023-06-02 19:15:34 -0400

    Proper animation list extensions

--- src/main/kotlin/org/teamvoided/voidlib/vui/v2/animation/Animation.kt
+++ src/main/kotlin/org/teamvoided/voidlib/vui/v2/animation/Animation.kt
@@ -12,40 +12,6 @@ class Animation<T>(
 ) {
     private var delta = 0f
 
-    companion object ListExtensions {
-        fun <T: List<Animation<A>>, A> T.forwards() {
-            forEach { it.forwards() }
-        }
-
-        fun <T: List<Animation<*>>> T.forwards() {
-            forEach { it.forwards() }
-        }
-
-        fun <T: List<Animation<A>>, A> T.backwards() {
-            forEach { it.backwards() }
-        }
-
-        fun <T: List<Animation<*>>> T.backwards() {
-            forEach { it.backwards() }
-        }
-
-        fun <T: List<Animation<A>>, A> T.reverse() {
-            forEach { it.reverse() }
-        }
-
-        fun <T: List<Animation<*>>> T.reverse() {
-            forEach { it.reverse() }
-        }
-
-        fun <T: List<Animation<A>>, A> T.loop(looping: Boolean) {
-            forEach { it.loop(looping) }
-        }
-
-        fun <T: List<Animation<*>>> T.loop(looping: Boolean) {
-            forEach { it.loop(looping) }
-        }
-    }
-
     fun update(delta: Float) {
         if (this.delta == this.direction.target) {
             if (looping) reverse()
---
+++ src/main/kotlin/org/teamvoided/voidlib/vui/v2/animation/AnimationListExt.kt
@@ -0,0 +1,37 @@
+package org.teamvoided.voidlib.vui.v2.animation
+
+object ListExtAny {
+    fun <T: List<Animation<*>>> T.forwards() {
+        forEach { it.forwards() }
+    }
+
+    fun <T: List<Animation<*>>> T.backwards() {
+        forEach { it.backwards() }
+    }
+
+    fun <T: List<Animation<*>>> T.reverse() {
+        forEach { it.reverse() }
+    }
+
+    fun <T: List<Animation<*>>> T.loop(looping: Boolean) {
+        forEach { it.loop(looping) }
+    }
+}
+
+object ListExtSpecific {
+    fun <T: List<Animation<A>>, A> T.forwards() {
+        forEach { it.forwards() }
+    }
+
+    fun <T: List<Animation<A>>, A> T.backwards() {
+        forEach { it.backwards() }
+    }
+
+    fun <T: List<Animation<A>>, A> T.reverse() {
+        forEach { it.reverse() }
+    }
+
+    fun <T: List<Animation<A>>, A> T.loop(looping: Boolean) {
+        forEach { it.loop(looping) }
+    }
+}
