aba71004c58c85eeefc87c1cd2dd2cf653f4b75c
BrokenFuse <brokenfuse.self@gmail.com>
2023-06-02 21:35:41 -0400

    Remove debug logs or something

--- src/main/kotlin/org/teamvoided/voidlib/vui/v2/node/ColorNode.kt
+++ src/main/kotlin/org/teamvoided/voidlib/vui/v2/node/ColorNode.kt
@@ -1,6 +1,5 @@
 package org.teamvoided.voidlib.vui.v2.node
 
-import org.teamvoided.voidlib.LOGGER
 import org.teamvoided.voidlib.core.ARGB
 import org.teamvoided.voidlib.core.datastructures.Vec2i
 import org.teamvoided.voidlib.vui.v2.DrawHelper
@@ -15,11 +14,6 @@ class ColorNode(var color: ARGB) : Node() {
     constructor(pos: Vec2i, size: Vec2i, color: ARGB) : this(color) {
         this.pos = pos
         this.size = size
-    }
-
-    override fun onKeyPress(event: Event.InputEvent.KeyPressEvent) {
-        LOGGER.info("$globalPos $size")
-        LOGGER.info("color$color int-color${color.toInt()}")
     }
 
     override fun draw(event: Event.LogicalEvent.DrawEvent) {
