e57ec7a612e53bf3125da31853e87694c4436afd
theendercore <theendercore@gmail.com>
2023-09-10 18:17:06 +0300

    clickable text, kind of

--- modules/vui/src/main/kotlin/org/teamvoided/voidlib/vui/impl/screen/EditorScreen.kt
+++ modules/vui/src/main/kotlin/org/teamvoided/voidlib/vui/impl/screen/EditorScreen.kt
@@ -1,7 +1,12 @@
 package org.teamvoided.voidlib.vui.impl.screen
 
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.HoverEvent
+import net.minecraft.text.Style
 import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import org.teamvoided.voidlib.core.ARGB
+import org.teamvoided.voidlib.core.LOGGER
 import org.teamvoided.voidlib.core.d
 import org.teamvoided.voidlib.core.datastructures.vector.Vec2d
 import org.teamvoided.voidlib.core.datastructures.vector.Vec2i
@@ -11,52 +16,53 @@ import org.teamvoided.voidlib.vui.v2.animation.Interpolator
 import org.teamvoided.voidlib.vui.v2.node.*
 import org.teamvoided.voidlib.vui.v2.screen.VoidUIAdapter
 import org.teamvoided.voidlib.vui.v2.screen.VuiScreen
+import java.util.function.UnaryOperator
 import kotlin.random.Random
 
-class EditorScreen: VuiScreen<BoxNode>(Text.literal("Vui Editor")) {
+class EditorScreen : VuiScreen<BoxNode>(Text.literal("Vui Editor")) {
     override val uiAdapter = VoidUIAdapter.create(this) { pos, size -> BoxNode(pos, size, ARGB(255, 0, 0, 0)) }
 
-    val boxNode = BoxNode(Vec2i(0,0), Vec2i(100, 100), ARGB(255, 0, 100, 200))
-/*    val animatedNode = AnimatedNode(
-//        boxNode
-//    ) { node ->
-//        node as BoxNode
-//        listOf(
-//            Animation(
-//                5,
-//                Animation.Direction.Forwards,
-//                true,
-//                InterpolatedProperty(node.topLeftColor, Interpolator.colorInterpolator) { node.topLeftColor = it },
-//                EasingFunction.sine,
-//                ARGB(255, 200, 200, 200)
-//            ),
-//            Animation(
-//                5,
-//                Animation.Direction.Forwards,
-//                true,
-//                InterpolatedProperty(node.topRightColor, Interpolator.colorInterpolator) { node.topRightColor = it },
-//                EasingFunction.exponential,
-//                ARGB(255, 240, 200, 200)
-//            ),
-//            Animation(
-//                5,
-//                Animation.Direction.Forwards,
-//                true,
-//                InterpolatedProperty(node.bottomLeftColor, Interpolator.colorInterpolator) { node.bottomLeftColor = it },
-//                EasingFunction.quartic,
-//                ARGB(255, 210, 180, 80)
-//            ),
-//            Animation(
-//                5,
-//                Animation.Direction.Forwards,
-//                true,
-//                InterpolatedProperty(node.bottomRightColor, Interpolator.colorInterpolator) { node.bottomRightColor = it },
-//                EasingFunction.linear,
-//                ARGB(255, 100, 200, 40)
-//            )
-//        )
-//    }
- */
+    val boxNode = BoxNode(Vec2i(0, 0), Vec2i(100, 100), ARGB(255, 0, 100, 200))
+    /*    val animatedNode = AnimatedNode(
+            boxNode
+        ) { node ->
+            node as BoxNode
+            listOf(
+                Animation(
+                    5,
+                    Animation.Direction.Forwards,
+                    true,
+                    InterpolatedProperty(node.topLeftColor, Interpolator.colorInterpolator) { node.topLeftColor = it },
+                    EasingFunction.sine,
+                    ARGB(255, 200, 200, 200)
+                ),
+                Animation(
+                    5,
+                    Animation.Direction.Forwards,
+                    true,
+                    InterpolatedProperty(node.topRightColor, Interpolator.colorInterpolator) { node.topRightColor = it },
+                    EasingFunction.exponential,
+                    ARGB(255, 240, 200, 200)
+                ),
+                Animation(
+                    5,
+                    Animation.Direction.Forwards,
+                    true,
+                    InterpolatedProperty(node.bottomLeftColor, Interpolator.colorInterpolator) { node.bottomLeftColor = it },
+                    EasingFunction.quartic,
+                    ARGB(255, 210, 180, 80)
+                ),
+                Animation(
+                    5,
+                    Animation.Direction.Forwards,
+                    true,
+                    InterpolatedProperty(node.bottomRightColor, Interpolator.colorInterpolator) { node.bottomRightColor = it },
+                    EasingFunction.linear,
+                    ARGB(255, 100, 200, 40)
+                )
+            )
+        }
+     */
 
     val button = ButtonNode(Vec2i(100, 100), Vec2i(200, 50), Text.literal("A Button"))
 
@@ -68,13 +74,28 @@ class EditorScreen: VuiScreen<BoxNode>(Text.literal("Vui Editor")) {
 
     val container = ContainerNode(Vec2i(500, 500), Vec2i(500, 500))
 
-    val textNode = TextNode(Vec2i(400, 400), Vec2i(0, 1), Text.of("something"), ARGB(255, 255, 255))
+    val textNode = TextNode(Vec2i(400, 400), Vec2i(0, 1), Text.of("something"), ARGB(255, 255, 255), this)
     val movNode = MovableNode(BoxNode(Vec2i(0, 0), Vec2i(50, 50), ARGB(255, 255, 255)))
 
+    val clickableLink = TextNode(
+        Vec2i(100, 25), Vec2i(0, 1),
+        Text.literal("heyo ").formatted(Formatting.RED).append(
+            Text.literal("Click here!").styled {
+                it.withColor(Formatting.GREEN)
+                    .withClickEvent(ClickEvent(ClickEvent.Action.OPEN_URL, "https://google.com"))
+                    .withHoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to link")))
+            }
+        ),
+        this
+    )
+
     override fun vuiInit() {
+
         //if there are no nodes the window will never flush out the minecraft loading screen cuz there's nothing to render on top of it
         //Add nodes here
+
         button.buttonPressCallback += {
+            LOGGER.info("---")
             val delta = Random.nextDouble(0.0, 1.0)
             val easing = EasingFunction.sine
             val mousePos = Vec2d(
@@ -103,6 +124,7 @@ class EditorScreen: VuiScreen<BoxNode>(Text.literal("Vui Editor")) {
         root.addChild(container)
 
         root.addChild(textNode)
+        root.addChild(clickableLink)
 
         container.debugBox = true
         container.addChild(movNode)
--- modules/vui/src/main/kotlin/org/teamvoided/voidlib/vui/v2/node/TextNode.kt
+++ modules/vui/src/main/kotlin/org/teamvoided/voidlib/vui/v2/node/TextNode.kt
@@ -1,18 +1,38 @@
 package org.teamvoided.voidlib.vui.v2.node
 
+import com.google.common.collect.Sets
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.screen.ConfirmLinkScreen
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.Style
 import net.minecraft.text.Text
+import net.minecraft.util.Util
+import org.slf4j.LoggerFactory
 import org.teamvoided.voidlib.core.ARGB
 import org.teamvoided.voidlib.core.datastructures.vector.Vec2i
 import org.teamvoided.voidlib.core.f
 import org.teamvoided.voidlib.vui.v2.event.ui.Event
 import org.teamvoided.voidlib.vui.v2.rendering.Pencil
+import java.io.File
+import java.net.URI
+import java.net.URISyntaxException
 
-open class TextNode(var text: Text, var textColor: ARGB): Node() {
-    constructor(pos: Vec2i, text: Text, textColor: ARGB): this(text, textColor) {
+open class TextNode(var text: Text, var textColor: ARGB, val screen: Screen) : Node() {
+    private val allowedProtocols: Set<String> = Sets.newHashSet(*arrayOf("http", "https"))
+    private val LOGGER = LoggerFactory.getLogger("TextNode")
+    private var clickedLink: URI? = null
+
+    constructor(pos: Vec2i, text: Text, textColor: ARGB, screen: Screen) : this(text, textColor, screen) {
         this.pos = pos
     }
 
-    constructor(pos: Vec2i, size: Vec2i, text: Text, textColor: ARGB): this(text, textColor) {
+    constructor(pos: Vec2i, size: Vec2i, text: Text, textColor: ARGB, screen: Screen) : this(text, textColor, screen) {
+        this.pos = pos
+        this.size = size
+    }
+
+    constructor(pos: Vec2i, size: Vec2i, text: Text, screen: Screen) : this(text, ARGB(255, 255, 255), screen) {
         this.pos = pos
         this.size = size
     }
@@ -20,4 +40,56 @@ open class TextNode(var text: Text, var textColor: ARGB): Node() {
     override fun draw(event: Event.LogicalEvent.DrawEvent) {
         Pencil.drawText(event.drawContext, text, globalPos, size.y.f, textColor)
     }
+
+    override fun onMousePress(event: Event.InputEvent.MousePressEvent) {
+        if (this.isTouching(event.pos)) {
+            text.siblings.forEach { handleTextClick(it.style) }
+        }
+    }
+
+    open fun handleTextClick(style: Style): Boolean {
+        val client = MinecraftClient.getInstance()
+
+        val clickEvent = style.clickEvent
+        if (clickEvent != null) {
+            val value = clickEvent.value
+            when (clickEvent.action) {
+                ClickEvent.Action.OPEN_URL -> {
+                    if (!client.options.chatLinks.value) return@handleTextClick false
+                    try {
+                        val uRI = URI(value)
+                        val string = uRI.scheme ?: throw URISyntaxException(value, "Missing protocol")
+                        if (!allowedProtocols.contains(string.lowercase()))
+                            throw URISyntaxException(value, "Unsupported protocol: " + string.lowercase())
+                        if (client.options.chatLinksPrompt.value) {
+                            clickedLink = uRI
+                            client.setScreen(ConfirmLinkScreen({ confirmLink(it) }, value, false))
+                        } else openLink(uRI)
+
+                    } catch (var5: URISyntaxException) {
+                        LOGGER.error("Can't open url for {}", clickEvent, var5)
+                    }
+                }
+
+                ClickEvent.Action.OPEN_FILE -> openLink(File(value).toURI())
+                ClickEvent.Action.COPY_TO_CLIPBOARD -> client.keyboard.clipboard = value
+                else -> {
+                    LOGGER.error("Don't know how to handle {}", clickEvent)
+                }
+            }
+            return true
+        }
+        return false
+
+    }
+
+    private fun openLink(uRI: URI) = Util.getOperatingSystem().open(uRI)
+
+    private fun confirmLink(bl: Boolean) {
+        if (bl) openLink(clickedLink!!)
+        clickedLink = null
+        MinecraftClient.getInstance().setScreen(screen)
+    }
+
+
 }
