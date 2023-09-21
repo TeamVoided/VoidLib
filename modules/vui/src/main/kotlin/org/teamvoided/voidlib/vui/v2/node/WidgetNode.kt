235fd1e491aa9a493480b025558c42d37880876e
BrokenFuse <brokenfuse.self@gmail.com>
2023-09-21 14:16:13 -0400

    Widget Wrapper

--- modules/vui/src/main/kotlin/org/teamvoided/voidlib/vui/v2/node/WidgetNode.kt
+++ modules/vui/src/main/kotlin/org/teamvoided/voidlib/vui/v2/node/WidgetNode.kt
@@ -1,38 +1,75 @@
 package org.teamvoided.voidlib.vui.v2.node
 
-import net.minecraft.client.gui.widget.ClickableWidget
+import net.minecraft.client.gui.Drawable
+import net.minecraft.client.gui.Element
+import net.minecraft.client.gui.widget.Widget
 import org.teamvoided.voidlib.core.datastructures.vector.Vec2i
 import org.teamvoided.voidlib.vui.v2.event.ui.Event
 
-class WidgetNode(var widget : ClickableWidget) : Node() {
-    constructor(pos: Vec2i, widget : ClickableWidget) : this(widget) {
-        this.pos = pos
-        widget.x = pos.x
-        widget.y = pos.y
+class WidgetNode(private val widgetConstructor: (pos: Vec2i, size: Vec2i) -> Widget) : Node() {
+    private var widget: Widget
+
+    init {
+        this.widget = widgetConstructor(pos, size)
+
+        resizeCallback += {
+            widget = widgetConstructor(pos, size)
+        }
     }
 
-    constructor(pos: Vec2i, size: Vec2i, widget : ClickableWidget) : this(pos, widget) {
+    constructor(pos: Vec2i, widget: (pos: Vec2i, size: Vec2i) -> Widget) : this(widget) {
+        this.pos = pos
+        this.widget = widget(pos, size)
+    }
+
+    constructor(pos: Vec2i, size: Vec2i, widget: (pos: Vec2i, size: Vec2i) -> Widget) : this(widget) {
         this.size = size
+        this.widget = widget(pos, size)
     }
 
     override fun draw(event: Event.LogicalEvent.DrawEvent) {
         event.ensurePreChild {
             val context = event.drawContext
-            widget.render(context.vanillaInstance, context.mousePos.x, context.mousePos.y, context.delta)
+            if (widget is Drawable)
+                (widget as Drawable).render(context.vanillaInstance, context.mousePos.x, context.mousePos.y, context.delta)
         }
     }
 
-    override fun update(event: Event.LogicalEvent.UpdateEvent) {
-        event.ensurePreChild {  }
+    override fun onMousePress(event: Event.InputEvent.MousePressEvent) {
+        if (widget is Element)
+            (widget as Element).mouseClicked(event.pos.x, event.pos.y, event.button)
     }
 
-    override fun onMousePress(event: Event.InputEvent.MousePressEvent) {
-         widget.mouseClicked(event.pos.x, event.pos.y, event.button)
+    override fun onMouseRelease(event: Event.InputEvent.MouseReleaseEvent) {
+        if (widget is Element)
+            (widget as Element).mouseReleased(event.pos.x, event.pos.y, event.button)
+    }
+
+    override fun onMouseDrag(event: Event.InputEvent.MouseDragEvent) {
+        if (widget is Element) {
+            (widget as Element).isFocused = isTouching(event.pos)
+            (widget as Element).mouseDragged(event.pos.x, event.pos.y, event.button, event.delta.x, event.delta.y)
+            (widget as Element).mouseMoved(event.pos.x, event.pos.y)
+        }
+    }
+
+    override fun onMouseScroll(event: Event.InputEvent.MouseScrollEvent) {
+        if (widget is Element)
+            (widget as Element).mouseScrolled(event.pos.x, event.pos.y, event.amount)
     }
 
     override fun onKeyPress(event: Event.InputEvent.KeyPressEvent) {
-        widget.keyPressed(event.keyCode, event.scanCode, event.modifiers)
-
+        if (widget is Element)
+            (widget as Element).keyPressed(event.keyCode, event.scanCode, event.modifiers)
     }
 
+    override fun onKeyRelease(event: Event.InputEvent.KeyReleaseEvent) {
+        if (widget is Element)
+            (widget as Element).keyReleased(event.keyCode, event.scanCode, event.modifiers)
+    }
+
+    override fun onCharTyped(event: Event.InputEvent.CharTypedEvent) {
+        if (widget is Element)
+            (widget as Element).charTyped(event.char, event.modifiers)
+    }
 }
