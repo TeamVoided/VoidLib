67c655e0a0bb4c2af0a4d10f09181ef37c15b978
theendercore <theendercore@gmail.com>
2023-09-24 22:01:11 +0300

    - fixed formatting

--- modules/networking/src/main/kotlin/org/teamvoided/voidlib/networking/Packets.kt
+++ modules/networking/src/main/kotlin/org/teamvoided/voidlib/networking/Packets.kt
@@ -7,12 +7,11 @@ import net.minecraft.network.PacketByteBuf
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Identifier
 
+
+@Suppress("unused", "MemberVisibilityCanBePrivate")
 object Packets {
     fun <PacketData> sendToClient(
-        chainId: Identifier,
-        player: ServerPlayerEntity,
-        packet: S2CVoidPacket<PacketData>,
-        data: PacketData
+        chainId: Identifier, player: ServerPlayerEntity, packet: S2CVoidPacket<PacketData>, data: PacketData,
     ) {
         val buf = createBuffer()
         packet.encode(buf, data)
@@ -20,88 +19,61 @@ object Packets {
     }
 
     fun <PacketData> sendToClient(
-        player: ServerPlayerEntity,
-        packet: ChainS2CVoidPacket<PacketData>,
-        data: PacketData
+        player: ServerPlayerEntity, packet: ChainS2CVoidPacket<PacketData>, data: PacketData,
     ) {
         val buf = createBuffer()
         packet.encode(buf, data)
         ServerPlayNetworking.send(player, packet.chainId, buf)
     }
 
-    fun <PacketData> sendToServer(
-        chainId: Identifier,
-        packet: C2SVoidPacket<PacketData>,
-        data: PacketData
-    ) {
+    fun <PacketData> sendToServer(chainId: Identifier, packet: C2SVoidPacket<PacketData>, data: PacketData) {
         val buf = PacketByteBuf(Unpooled.buffer())
         packet.encode(buf, data)
         ClientPlayNetworking.send(chainId, buf)
     }
 
     @JvmName("staticSendToServer")
-    fun <PacketData> sendToServer(
-        packet: ChainC2SVoidPacket<PacketData>,
-        data: PacketData
-    ) {
+    fun <PacketData> sendToServer(packet: ChainC2SVoidPacket<PacketData>, data: PacketData) {
         val buf = PacketByteBuf(Unpooled.buffer())
         packet.encode(buf, data)
         ClientPlayNetworking.send(packet.chainId, buf)
     }
 
     fun <PacketData> S2CVoidPacket<PacketData>.sendToClient(
-        chainId: Identifier,
-        player: ServerPlayerEntity,
-        data: PacketData
+        chainId: Identifier, player: ServerPlayerEntity, data: PacketData,
     ) = sendToClient(chainId, player, this, data)
 
-    fun <PacketData> ChainS2CVoidPacket<PacketData>.sendToClient(
-        player: ServerPlayerEntity,
-        data: PacketData
-    ) = sendToClient(player, this, data)
+    fun <PacketData> ChainS2CVoidPacket<PacketData>.sendToClient(player: ServerPlayerEntity, data: PacketData) =
+        sendToClient(player, this, data)
 
-    fun <PacketData> C2SVoidPacket<PacketData>.sendToServer(
-        chainId: Identifier,
-        data: PacketData
-    ) = sendToServer(chainId, this, data)
+    fun <PacketData> C2SVoidPacket<PacketData>.sendToServer(chainId: Identifier, data: PacketData) =
+        sendToServer(chainId, this, data)
 
-    fun <PacketData> ChainC2SVoidPacket<PacketData>.sendToServer(
-        data: PacketData
-    ) = sendToServer(this, data)
+    fun <PacketData> ChainC2SVoidPacket<PacketData>.sendToServer(data: PacketData) = sendToServer(this, data)
 
-    fun <PacketData> openChainClient(
-        chain: Chain, packet: S2CVoidPacket<PacketData>
-    ) {
+    fun <PacketData> openChainClient(chain: Chain, packet: S2CVoidPacket<PacketData>) {
         ClientPlayNetworking.registerGlobalReceiver(chain.chainId) { client, handler, buf, responseSender ->
             packet.handleS2C(client, handler, buf, responseSender)
         }
     }
 
-    fun <PacketData> openChainServer(
-        chain: Chain, packet: C2SVoidPacket<PacketData>
-    ) {
+    fun <PacketData> openChainServer(chain: Chain, packet: C2SVoidPacket<PacketData>) {
         ServerPlayNetworking.registerGlobalReceiver(chain.chainId) { server, player, handler, buf, responseSender ->
             packet.handleC2S(server, player, handler, buf, responseSender)
         }
     }
 
-    fun <PacketData> Chain.open(packet: S2CVoidPacket<PacketData>) =
-        openChainClient(this, packet)
+    fun <PacketData> Chain.open(packet: S2CVoidPacket<PacketData>) = openChainClient(this, packet)
 
-    fun <PacketData> Chain.openClient(packet: S2CVoidPacket<PacketData>) =
-        openChainClient(this, packet)
+    fun <PacketData> Chain.openClient(packet: S2CVoidPacket<PacketData>) = openChainClient(this, packet)
 
-    fun <PacketData> Chain.open(packet: C2SVoidPacket<PacketData>) =
-        openChainServer(this, packet)
+    fun <PacketData> Chain.open(packet: C2SVoidPacket<PacketData>) = openChainServer(this, packet)
 
-    fun <PacketData> Chain.openServer(packet: C2SVoidPacket<PacketData>) =
-        openChainServer(this, packet)
+    fun <PacketData> Chain.openServer(packet: C2SVoidPacket<PacketData>) = openChainServer(this, packet)
 
-    fun <PacketData> ChainC2SVoidPacket<PacketData>.open() =
-        openChainServer(this, this)
+    fun <PacketData> ChainC2SVoidPacket<PacketData>.open() = openChainServer(this, this)
 
-    fun <PacketData> ChainS2CVoidPacket<PacketData>.open() =
-        openChainClient(this, this)
+    fun <PacketData> ChainS2CVoidPacket<PacketData>.open() = openChainClient(this, this)
 
     fun createBuffer() = PacketByteBuf(Unpooled.buffer())
 }
