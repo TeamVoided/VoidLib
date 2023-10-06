562406f5ba8b0846d49ca1ab10dc63c692382149
theendercore <theendercore@gmail.com>
2023-10-06 18:45:05 +0300

    - updated ItemNbt helper - version bump

--- modules/core/src/main/kotlin/org/teamvoided/voidlib/core/nbt/ItemNbt.kt
+++ modules/core/src/main/kotlin/org/teamvoided/voidlib/core/nbt/ItemNbt.kt
@@ -9,65 +9,63 @@ import net.minecraft.nbt.NbtList
 import org.teamvoided.voidlib.core.i
 import java.util.*
 
-fun put(key: String, element: NbtElement, stack: ItemStack): NbtElement? = stack.orCreateNbt.put(key, element)
-fun putByte(key: String, byte: Byte, stack: ItemStack) = stack.orCreateNbt.putByte(key, byte)
-fun putShort(key: String, short: Short, stack: ItemStack) = stack.orCreateNbt.putShort(key, short)
-fun putInt(key: String, int: Int, stack: ItemStack) = stack.orCreateNbt.putInt(key, int)
-fun putLong(key: String, long: Long, stack: ItemStack) = stack.orCreateNbt.putLong(key, long)
-fun putUuid(key: String, uuid: UUID, stack: ItemStack) = stack.orCreateNbt.putUuid(key, uuid)
-fun getUuid(key: String, stack: ItemStack): UUID = stack.orCreateNbt.getUuid(key)
-fun containsUuid(key: String, stack: ItemStack): Boolean = stack.orCreateNbt.containsUuid(key)
-fun putFloat(key: String, float: Float, stack: ItemStack) = stack.orCreateNbt.putFloat(key, float)
-fun putDouble(key: String, double: Double, stack: ItemStack) = stack.orCreateNbt.putDouble(key, double)
-fun putString(key: String, string: String, stack: ItemStack) = stack.orCreateNbt.putString(key, string)
-fun putByteArray(key: String, byteArray: ByteArray, stack: ItemStack) = stack.orCreateNbt.putByteArray(key, byteArray)
-fun putByteArray(key: String, byteArray: List<Byte>, stack: ItemStack) = stack.orCreateNbt.putByteArray(key, byteArray)
-fun putIntArray(key: String, intArray: IntArray, stack: ItemStack) = stack.orCreateNbt.putIntArray(key, intArray)
-fun putIntArray(key: String, intArray: List<Int>, stack: ItemStack) = stack.orCreateNbt.putIntArray(key, intArray)
-fun putLongArray(key: String, longArray: LongArray, stack: ItemStack) = stack.orCreateNbt.putLongArray(key, longArray)
-fun putLongArray(key: String, longArray: List<Long>, stack: ItemStack) = stack.orCreateNbt.putLongArray(key, longArray)
-fun putBoolean(key: String, boolean: Boolean, stack: ItemStack) = stack.orCreateNbt.putBoolean(key, boolean)
-fun get(key: String, stack: ItemStack): NbtElement? = stack.orCreateNbt[key]
-fun getType(key: String, stack: ItemStack): Byte = stack.orCreateNbt.getType(key)
-fun contains(key: String, stack: ItemStack): Boolean = stack.orCreateNbt.contains(key)
-
-// NbtTypes.class
-fun contains(key: String, type: Int, stack: ItemStack): Boolean = stack.orCreateNbt.contains(key, type)
-fun getByte(key: String, stack: ItemStack): Byte = stack.orCreateNbt.getByte(key)
-fun getShort(key: String, stack: ItemStack): Short = stack.orCreateNbt.getShort(key)
-fun getInt(key: String, stack: ItemStack): Int = stack.orCreateNbt.getInt(key)
-fun getLong(key: String, stack: ItemStack): Long = stack.orCreateNbt.getLong(key)
-fun getFloat(key: String, stack: ItemStack): Float = stack.orCreateNbt.getFloat(key)
-fun getDouble(key: String, stack: ItemStack): Double = stack.orCreateNbt.getDouble(key)
-fun getString(key: String, stack: ItemStack): String = stack.orCreateNbt.getString(key)
-fun getByteArray(key: String, stack: ItemStack): ByteArray = stack.orCreateNbt.getByteArray(key)
-fun getIntArray(key: String, stack: ItemStack): IntArray = stack.orCreateNbt.getIntArray(key)
-fun getLongArray(key: String, stack: ItemStack): LongArray = stack.orCreateNbt.getLongArray(key)
-fun getCompound(key: String, stack: ItemStack): NbtCompound = stack.orCreateNbt.getCompound(key)
-
-// NbtTypes.class
-fun getList(key: String, type: Int, stack: ItemStack): NbtList = stack.orCreateNbt.getList(key, type)
-fun getBoolean(key: String, stack: ItemStack): Boolean = stack.orCreateNbt.getBoolean(key)
-fun remove(key: String, stack: ItemStack) = stack.orCreateNbt.remove(key)
-fun isEmpty(stack: ItemStack): Boolean = stack.orCreateNbt.isEmpty
-fun subNbt(key: String, stack: ItemStack): NbtCompound = stack.getOrCreateSubNbt(key)
+fun ItemStack.put(key: String, element: NbtElement): NbtElement? = this.orCreateNbt.put(key, element)
+fun ItemStack.putByte(key: String, byte: Byte) = this.orCreateNbt.putByte(key, byte)
+fun ItemStack.putShort(key: String, short: Short) = this.orCreateNbt.putShort(key, short)
+fun ItemStack.putInt(key: String, int: Int) = this.orCreateNbt.putInt(key, int)
+fun ItemStack.putLong(key: String, long: Long) = this.orCreateNbt.putLong(key, long)
+fun ItemStack.putUuid(key: String, uuid: UUID) = this.orCreateNbt.putUuid(key, uuid)
+fun ItemStack.getUuid(key: String): UUID = this.orCreateNbt.getUuid(key)
+fun ItemStack.containsUuid(key: String): Boolean = this.orCreateNbt.containsUuid(key)
+fun ItemStack.putFloat(key: String, float: Float) = this.orCreateNbt.putFloat(key, float)
+fun ItemStack.putDouble(key: String, double: Double) = this.orCreateNbt.putDouble(key, double)
+fun ItemStack.putString(key: String, string: String) = this.orCreateNbt.putString(key, string)
+fun ItemStack.putByteArray(key: String, byteArray: ByteArray) = this.orCreateNbt.putByteArray(key, byteArray)
+fun ItemStack.putByteArray(key: String, byteArray: List<Byte>) = this.orCreateNbt.putByteArray(key, byteArray)
+fun ItemStack.putIntArray(key: String, intArray: IntArray) = this.orCreateNbt.putIntArray(key, intArray)
+fun ItemStack.putIntArray(key: String, intArray: List<Int>) = this.orCreateNbt.putIntArray(key, intArray)
+fun ItemStack.putLongArray(key: String, longArray: LongArray) = this.orCreateNbt.putLongArray(key, longArray)
+fun ItemStack.putLongArray(key: String, longArray: List<Long>) = this.orCreateNbt.putLongArray(key, longArray)
+fun ItemStack.putBoolean(key: String, boolean: Boolean) = this.orCreateNbt.putBoolean(key, boolean)
+fun ItemStack.get(key: String): NbtElement? = this.orCreateNbt[key]
+fun ItemStack.getType(key: String): Byte = this.orCreateNbt.getType(key)
+fun ItemStack.contains(key: String): Boolean = this.orCreateNbt.contains(key)
+fun ItemStack.contains(key: String, type: Type): Boolean = this.orCreateNbt.contains(key, type.id)
+fun ItemStack.getByte(key: String): Byte = this.orCreateNbt.getByte(key)
+fun ItemStack.getShort(key: String): Short = this.orCreateNbt.getShort(key)
+fun ItemStack.getInt(key: String): Int = this.orCreateNbt.getInt(key)
+fun ItemStack.getLong(key: String): Long = this.orCreateNbt.getLong(key)
+fun ItemStack.getFloat(key: String): Float = this.orCreateNbt.getFloat(key)
+fun ItemStack.getDouble(key: String): Double = this.orCreateNbt.getDouble(key)
+fun ItemStack.getString(key: String): String = this.orCreateNbt.getString(key)
+fun ItemStack.getByteArray(key: String): ByteArray = this.orCreateNbt.getByteArray(key)
+fun ItemStack.getIntArray(key: String): IntArray = this.orCreateNbt.getIntArray(key)
+fun ItemStack.getLongArray(key: String): LongArray = this.orCreateNbt.getLongArray(key)
+fun ItemStack.getCompound(key: String): NbtCompound = this.orCreateNbt.getCompound(key)
+fun ItemStack.getList(key: String, type: Type): NbtList = this.orCreateNbt.getList(key, type.id)
+fun ItemStack.getBoolean(key: String): Boolean = this.orCreateNbt.getBoolean(key)
+fun ItemStack.remove(key: String) = this.orCreateNbt.remove(key)
+fun ItemStack.isNbtEmpty(): Boolean = this.orCreateNbt.isEmpty
+fun ItemStack.subNbt(key: String): NbtCompound = this.getOrCreateSubNbt(key)
 
 
 
 enum class Type(val id: Int) {
-    END_TYPE(NbtElement.END_TYPE.i),
-    BYTE_TYPE(NbtElement.BYTE_TYPE.i),
-    SHORT_TYPE(NbtElement.SHORT_TYPE.i),
-    INT_TYPE(NbtElement.INT_TYPE.i),
-    LONG_TYPE(NbtElement.LONG_TYPE.i),
-    FLOAT_TYPE(NbtElement.FLOAT_TYPE.i),
-    DOUBLE_TYPE(NbtElement.DOUBLE_TYPE.i),
-    BYTE_ARRAY_TYPE(NbtElement.BYTE_ARRAY_TYPE.i),
-    STRING_TYPE(NbtElement.STRING_TYPE.i),
-    LIST_TYPE(NbtElement.LIST_TYPE.i),
-    COMPOUND_TYPE(NbtElement.COMPOUND_TYPE.i),
-    INT_ARRAY_TYPE(NbtElement.INT_ARRAY_TYPE.i),
-    LONG_ARRAY_TYPE(NbtElement.LONG_ARRAY_TYPE.i),
-    NUMBER_TYPE(NbtElement.NUMBER_TYPE.i);
+    END_TYPE(NbtElement.END_TYPE),
+    BYTE_TYPE(NbtElement.BYTE_TYPE),
+    SHORT_TYPE(NbtElement.SHORT_TYPE),
+    INT_TYPE(NbtElement.INT_TYPE),
+    LONG_TYPE(NbtElement.LONG_TYPE),
+    FLOAT_TYPE(NbtElement.FLOAT_TYPE),
+    DOUBLE_TYPE(NbtElement.DOUBLE_TYPE),
+    BYTE_ARRAY_TYPE(NbtElement.BYTE_ARRAY_TYPE),
+    STRING_TYPE(NbtElement.STRING_TYPE),
+    LIST_TYPE(NbtElement.LIST_TYPE),
+    COMPOUND_TYPE(NbtElement.COMPOUND_TYPE),
+    INT_ARRAY_TYPE(NbtElement.INT_ARRAY_TYPE),
+    LONG_ARRAY_TYPE(NbtElement.LONG_ARRAY_TYPE),
+    NUMBER_TYPE(NbtElement.NUMBER_TYPE);
+    constructor(id:Byte): this(id.i)
+
 }
 
