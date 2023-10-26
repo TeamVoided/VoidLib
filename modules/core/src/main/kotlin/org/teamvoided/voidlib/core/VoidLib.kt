6d8f40919ed6a89278216aedb3bd2d6b7b31f983
theendercore <theendercore@gmail.com>
2023-10-26 20:26:58 +0300

    - added internal status to core (finnaly)

--- modules/core/src/main/kotlin/org/teamvoided/voidlib/core/VoidLib.kt
+++ modules/core/src/main/kotlin/org/teamvoided/voidlib/core/VoidLib.kt
@@ -1,12 +1,16 @@
 package org.teamvoided.voidlib.core
 
 import net.minecraft.util.Identifier
+import org.jetbrains.annotations.ApiStatus
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+@ApiStatus.Internal
 const val MODID = "voidlib"
 
 @JvmField
+@ApiStatus.Internal
 val LOGGER: Logger = LoggerFactory.getLogger(MODID)
 
+@ApiStatus.Internal
 fun id(path: String): Identifier = Identifier(MODID, path)
